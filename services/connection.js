52a70155622cdf9142485fe61b86f7ce8ea8c352
Kapil-dharme <kapildharme676@gmail.com>
2026-01-04 18:36:11 +0530

    new commit

--- services/connection.js
+++ services/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
-let connecturl = process.env.MONGODB_URI || "mongodb://localhost:27017/PYQ-HUB"
-function connecttoserver() {
-    return mongoose.connect(connecturl).then(() => console.log("mongodb connected")).catch((err) => console.log(err))
+let connecturl =process.env.MONGODB_URI || "mongodb://localhost:27017/PYQ-HUB"
+async function connecttoserver() {
+    return await mongoose.connect(connecturl).then(() => console.log("mongodb connected")).catch((err) => console.log(err))
 }
 module.exports = { connecttoserver };
